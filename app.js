1718eb2f188bc7644e11002c1e76d49dade08685
JhairRoussell2 <jhairrmb3@gmail.com>
2025-03-11 19:40:00 -0500

    Se creó el array y la función para agregarle los nombres

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigos() {
+  let nombre = document.getElementById("amigo").value;
+  if (nombre == "") {
+    alert("Por favor, inserte un nombre.");
+    document.getElementById('amigo') = "";
+    return;
+  } 
+  if (amigos.includes(nombre)){
+    alert("El amigo ya está en la lista");
+    return;
+  }else {
+    amigos.push(nombre);
+    console.log("Se ha ingresado el amigo: " + amigos);
+    document.getElementById('amigo') = "";x
+  }
+}
